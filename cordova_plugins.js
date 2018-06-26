f89372773184b4e7464594c45f07d748d9185110
ecuaryan <ryanwillmore@gmail.com>
2018-06-26 01:11:42 -0600

    hope this works, it's been awhile

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -15,15 +15,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-ionic/www/common.js",
-        "id": "cordova-plugin-ionic.common",
-        "pluginId": "cordova-plugin-ionic",
-        "clobbers": [
-            "IonicCordova"
-        ],
-        "runs": true
-    },
-    {
         "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
         "id": "cordova-plugin-splashscreen.SplashScreen",
         "pluginId": "cordova-plugin-splashscreen",
@@ -61,26 +52,25 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/cordova-plugin-safariviewcontroller/www/SafariViewController.js",
-        "id": "cordova-plugin-safariviewcontroller.SafariViewController",
-        "pluginId": "cordova-plugin-safariviewcontroller",
+        "file": "plugins/cordova-plugin-ionic/www/common.js",
+        "id": "cordova-plugin-ionic.common",
+        "pluginId": "cordova-plugin-ionic",
         "clobbers": [
-            "SafariViewController"
-        ]
+            "IonicCordova"
+        ],
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-add-swift-support": "1.7.0",
-    "cordova-plugin-device": "1.1.4",
-    "cordova-plugin-ionic": "1.1.9",
-    "cordova-plugin-splashscreen": "4.0.3",
-    "cordova-plugin-statusbar": "2.2.2",
-    "cordova-plugin-whitelist": "1.3.1",
+    "cordova-plugin-device": "1.1.7",
+    "cordova-plugin-splashscreen": "4.1.0",
+    "cordova-plugin-statusbar": "2.4.2",
+    "cordova-plugin-whitelist": "1.3.3",
     "ionic-plugin-keyboard": "2.2.1",
-    "cordova-plugin-safariviewcontroller": "1.4.7",
-    "cordova-plugin-customurlscheme": "4.3.0"
+    "cordova-plugin-add-swift-support": "1.7.2",
+    "cordova-plugin-ionic": "1.1.9"
 }
 // BOTTOM OF METADATA
 });
